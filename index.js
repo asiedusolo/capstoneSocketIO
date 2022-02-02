4d6df3acfb42976473711b45c2eda25e4bb433d6
asiedusolo <solomon.asiedu@amalitech.org>
2022-02-02 14:00:58 +0000

    removing headers

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://letxchat.netlify.app",
-    "https://admin.socket.io/#/"
-  );
-  res.header(
-    "Acesss-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://letxchat.netlify.app",
+//     "https://admin.socket.io/#/"
+//   );
+//   res.header(
+//     "Acesss-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 const PORT = process.env.PORT || 8900;
 const server = app.listen(PORT);
 const socket = require("socket.io");
