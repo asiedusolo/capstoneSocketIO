05f76ca8368ff24967749ede3098f1242ff2dc26
asiedusolo <solomon.asiedu@amalitech.org>
2022-02-02 13:52:12 +0000

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { instrument } = require("@socket.io/admin-ui");
 const express = require("express");
 const app = express();
-// const cors = require("cors");
-// app.use(cors());
+const cors = require("cors");
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header(
